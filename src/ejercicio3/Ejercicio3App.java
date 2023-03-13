db67d80faeef70ef4a786818b4baee5be528b163
burnout131 <marcrg07@gmail.com>
2023-03-13 22:20:47 +0100

    Commit inicial de ejercicio 3 i 4

---
+++ src/ejercicio3/Ejercicio3App.java
@@ -0,0 +1,12 @@
+package ejercicio3;
+
+public class Ejercicio3App {
+
+	public static void main(String[] args) {
+		/*
+		 * PSEUDO CODIGO
+		 * articulo:(precio:10.50,cantidad:5)
+		 * */
+	}
+
+}
---
+++ src/ejercicio4/Ejercicio4App.java
@@ -0,0 +1,9 @@
+package ejercicio4;
+
+public class Ejercicio4App {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
